6932dfe26090492ae65068c2cd6d0700fbe03db7
fangasvsass <fffffangqi@gmail.com>
2020-12-11 17:09:05 +0800

    chore: update

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
@@ -1,23 +1,11 @@
 package com.dylanvann.fastimage;
 
 import android.text.TextUtils;
-import android.util.Log;
-import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.MemoryCategory;
-import com.bumptech.glide.Priority;
 import com.bumptech.glide.RequestManager;
-import com.bumptech.glide.load.MultiTransformation;
-import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.load.resource.bitmap.CircleCrop;
-import com.bumptech.glide.load.resource.bitmap.FitCenter;
 import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.ImageViewTarget;
-import com.bumptech.glide.request.target.Target;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewProps;
@@ -25,14 +13,10 @@ import com.facebook.react.uimanager.annotations.ReactProp;
 
 import javax.annotation.Nullable;
 
-import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
 
 class FastImageViewManager extends SimpleViewManager<FastImageViewWithUrl>  {
     private static final String REACT_CLASS = "FastImageView";
     private static RequestManager requestManager = null;
-    private RequestOptions circleCrop = RequestOptions.circleCropTransform();
-    private RequestOptions fitCenter = RequestOptions.fitCenterTransform();
-    private MultiTransformation multiTransformation = new MultiTransformation(new FitCenter(), new CircleCrop());
 
     @Override
     public String getName() {
@@ -49,9 +33,6 @@ class FastImageViewManager extends SimpleViewManager<FastImageViewWithUrl>  {
 
     @ReactProp(name = "source")
     public void setSrc(FastImageViewWithUrl view, @Nullable ReadableMap source) {
-        if (source == null) {
-            return;
-        }
         // Get the GlideUrl which contains header info.
         final FastImageSource imageSource = FastImageViewConverter.getImageSource(view.getContext(), source);
         view.glideUrl = imageSource;
@@ -70,6 +51,7 @@ class FastImageViewManager extends SimpleViewManager<FastImageViewWithUrl>  {
 
         FastImageViewWithUrl.ScaleType scaleType = FastImageViewConverter.getScaleType(view.resizeMode);
         view.setScaleType(scaleType);
+
 
         if (TextUtils.isEmpty(view.defaultSource)) {
 
